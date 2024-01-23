c68a1718113f5776417252ee75902e79e4f29528
Amirelo <80700398+Amirelo@users.noreply.github.com>
2024-01-23 08:30:53 +0700

    Add bottom navigation + icons

--- src/screens/main/MainNavigation.tsx
+++ src/screens/main/MainNavigation.tsx
@@ -1,15 +1,39 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
-import { HomeScreen, ExploreScreen } from './screens';
+import {HomeScreen, ExploreScreen} from './screens';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faHome, faCompass} from '@fortawesome/free-solid-svg-icons';
+import themes from '../../themes/themes';
+import lang from '../../language/lang';
 
 const tab = createBottomTabNavigator();
 
 const MainTab = () => {
   return (
     <NavigationContainer>
-      <tab.Navigator>
-        <tab.Screen name="Home" component={HomeScreen} />
-        <tab.Screen name="Explore" component={ExploreScreen} />
+      <tab.Navigator initialRouteName="Home">
+        <tab.Screen
+          options={{
+            headerTitle: lang['en'].appBarHome,
+            headerTitleAlign:'center',
+            tabBarIcon: ({color, size}) => (
+              <FontAwesomeIcon size={size} color={color} icon={faHome} />
+            ),
+            tabBarActiveTintColor: themes['defaultTheme'].primaryColor
+          }}
+          name="Home"
+          component={HomeScreen}
+        />
+        <tab.Screen
+        options={{
+          headerTitleAlign:'center',
+          tabBarIcon: ({color,size}) => (
+            <FontAwesomeIcon size={size} color={color} icon={faCompass}/>
+          ),
+          tabBarActiveTintColor: themes['defaultTheme'].primaryColor,
+        }}
+         name="Explore" 
+         component={ExploreScreen} />
       </tab.Navigator>
     </NavigationContainer>
   );
