ae8132ac30b04d9b8a077c7e89de1778c4246496
Amirelo <80700398+Amirelo@users.noreply.github.com>
2024-01-23 18:33:06 +0700

    Update product model

--- src/components/models/ProductModel.ts
+++ src/components/models/ProductModel.ts
@@ -1,13 +1,15 @@
 class ProductModel {
     name: string;
     price: number;
+    quantity: number;
     description: string;
     totalRating: number;
     categoryID: number;
     links: Array<string>;
-    constructor(name: string, price:number, description: string, totalRating: number, categoryID: number, links: Array<string>){
+    constructor(name: string, price:number, quantity:number, description: string, totalRating: number, categoryID: number, links: Array<string>){
         this.name = name;
         this.price = price;
+        this.quantity = quantity;
         this.description = description;
         this.totalRating = totalRating;
         this.categoryID = categoryID;
--- src/screens/main/screens/HomeScreen.tsx
+++ src/screens/main/screens/HomeScreen.tsx
@@ -33,7 +33,7 @@ const HomeScreen = () => {
   console.log(list);
 
   var productList = new Array<ProductModel>();
-  const testProduct = new ProductModel('Spark', 90, 'A bouquet', 2.5, 1, [
+  const testProduct = new ProductModel('Spark', 90, 3, 'A bouquet', 2.5, 1, [
     'https://cdn.pixabay.com/photo/2024/01/12/21/23/cortina-dampezzo-8504755_1280.jpg',
   ]);
   productList.push(testProduct);
