3260045c8e1b4899320d6e392f2a493dc0178926
Amirelo <80700398+Amirelo@users.noreply.github.com>
2024-01-28 21:44:10 +0700

    ExploreScreen: Fixed item go below BottomTabNavigation

--- src/screens/main/screens/ExploreScreen.tsx
+++ src/screens/main/screens/ExploreScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {FlatList, StyleSheet, View} from 'react-native';
+import {FlatList, ScrollView, StyleSheet, View} from 'react-native';
 import {
   CustomInput,
   ItemProduct,
@@ -19,92 +19,107 @@ import {CustomText} from '../../../components/atoms';
 
 const ExploreScreen = () => {
   const [isColumn, setIsColumn] = React.useState(false);
-  const [filteredList, setFilteredList] = React.useState<Array<ProductModel>>([]);
+  const [filteredList, setFilteredList] = React.useState<Array<ProductModel>>(
+    [],
+  );
 
   const onDisplayPressed = () => {
     setIsColumn(!isColumn);
   };
 
   const onSearch = (searchText: string) => {
-    var filtered = productList.filter(item =>   item.name.toLowerCase().includes(searchText.toLowerCase()))
-    setFilteredList(filtered)
-  }
+    var filtered = productList.filter(item =>
+      item.name.toLowerCase().includes(searchText.toLowerCase()),
+    );
+    setFilteredList(filtered);
+  };
 
   var productList = new Array<ProductModel>();
-  var testProduct = new ProductModel(1,'Spark', 35, 5, 'A bouquet', 2.4, 1, [
+  var testProduct = new ProductModel(1, 'Spark', 35, 5, 'A bouquet', 2.4, 1, [
     'https://images.pexels.com/photos/230129/pexels-photo-230129.jpeg',
   ]);
   productList.push(testProduct);
-  testProduct = new ProductModel(2,'Bomb', 75, 5, 'A bouquet', 2.4, 1, [
+  testProduct = new ProductModel(2, 'Bomb', 75, 5, 'A bouquet', 3.6, 1, [
     'https://images.pexels.com/photos/953057/pexels-photo-953057.jpeg',
   ]);
   productList.push(testProduct);
-  testProduct = new ProductModel(3,'Rot', 13, 5, 'A bouquet', 2.4, 1, [
+  testProduct = new ProductModel(3, 'Rot', 13, 5, 'A bouquet', 4.3, 1, [
     'https://images.pexels.com/photos/2099737/pexels-photo-2099737.jpeg',
   ]);
   productList.push(testProduct);
-  testProduct = new ProductModel(4,'Roto', 36, 5, 'A bouquet', 2.4, 1, [
+  testProduct = new ProductModel(4, 'Roto', 36, 5, 'A bouquet', 5, 1, [
     'https://images.pexels.com/photos/250716/pexels-photo-250716.jpeg',
   ]);
   productList.push(testProduct);
-  testProduct = new ProductModel(5,'Comb', 78, 5, 'A bouquet', 2.4, 1, [
+  testProduct = new ProductModel(5, 'Comb', 78, 5, 'A bouquet', 1.2, 1, [
     'https://images.pexels.com/photos/372166/pexels-photo-372166.jpeg',
   ]);
   productList.push(testProduct);
-  testProduct = new ProductModel(6,'Sap', 54, 5, 'A bouquet', 2.4, 1, [
+  testProduct = new ProductModel(6, 'Sap', 54, 5, 'A bouquet', 3.6, 1, [
     'https://images.pexels.com/photos/18011894/pexels-photo-18011894/free-photo-of-tea-glasses-on-windowsill.jpeg',
   ]);
   productList.push(testProduct);
 
-  React.useEffect(()=>{
-    setFilteredList(productList)
-  },[])
+  React.useEffect(() => {
+    setFilteredList(productList);
+  }, []);
 
   return (
-    <View style={styles.view}>
-      <CustomInput onChangeText={(text) => onSearch(text)} icon={faSearch} marginBottom={12}/>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-        <ItemRow justifyContent="flex-start">
-          <FontAwesomeIcon style={{marginRight: 4}} icon={faSliders} />
-          <CustomText type='subTitle'>Filter</CustomText>
-        </ItemRow>
+    <View style={{flex:1}}>
+      <View style={styles.view}>
+        <CustomInput
+          onChangeText={text => onSearch(text)}
+          icon={faSearch}
+          marginBottom={12}
+        />
+        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+          <ItemRow justifyContent="flex-start">
+            <FontAwesomeIcon style={{marginRight: 4}} icon={faSliders} />
+            <CustomText type="subTitle">Filter</CustomText>
+          </ItemRow>
 
-        <ItemRow justifyContent="flex-start">
-          <FontAwesomeIcon style={{marginRight: 4}} icon={faSort} />
-          <CustomText type='subTitle'>Sort</CustomText>
-        </ItemRow>
+          <ItemRow justifyContent="flex-start">
+            <FontAwesomeIcon style={{marginRight: 4}} icon={faSort} />
+            <CustomText type="subTitle">Sort</CustomText>
+          </ItemRow>
 
-      <CustomButton onPressed={onDisplayPressed}>
-        <ItemRow justifyContent="flex-start">
-          <FontAwesomeIcon style={{marginRight: 4}} icon={isColumn ? faSquareFull : faGripVertical} />
-          <CustomText type='subTitle'>{isColumn ? "Colm" : "Grid"}</CustomText>
-        </ItemRow>
-        </CustomButton>
+          <CustomButton onPressed={onDisplayPressed}>
+            <ItemRow justifyContent="flex-start">
+              <FontAwesomeIcon
+                style={{marginRight: 4}}
+                icon={isColumn ? faSquareFull : faGripVertical}
+              />
+              <CustomText type="subTitle">
+                {isColumn ? 'Colm' : 'Grid'}
+              </CustomText>
+            </ItemRow>
+          </CustomButton>
+        </View>
+        {isColumn == false ? (
+          <FlatList
+            columnWrapperStyle={{
+              justifyContent: 'space-around',
+              marginBottom: 24,
+            }}
+            style={{marginTop: 24}}
+            showsVerticalScrollIndicator={false}
+            numColumns={2}
+            data={filteredList}
+            keyExtractor={item => item.id.toString()}
+            renderItem={({item}) => <ItemProduct product={item} />}
+          />
+        ) : (
+          <FlatList
+            key={'#'}
+            contentContainerStyle={{gap: 16, paddingBottom:20}}
+            style={{marginTop: 24}}
+            showsVerticalScrollIndicator={false}
+            data={filteredList}
+            keyExtractor={item => item.id.toString()}
+            renderItem={({item}) => <ItemProductLong product={item} />}
+          />
+        )}
       </View>
-      {isColumn ==false ? (
-        <FlatList
-          columnWrapperStyle={{
-            justifyContent: 'space-around',
-            marginBottom: 24,
-          }}
-          style={{marginTop: 24}}
-          showsVerticalScrollIndicator={false}
-          numColumns={2}
-          data={filteredList}
-          keyExtractor={item => item.id.toString()}
-          renderItem={({item}) => <ItemProduct product={item} />}
-        />
-      ) : (
-        <FlatList
-          key={'#'}
-          contentContainerStyle={{gap: 16}}
-          style={{marginTop: 24}}
-          showsVerticalScrollIndicator={false}
-          data={filteredList}
-          keyExtractor={item => item.id.toString()}
-          renderItem={({item}) => <ItemProductLong product={item} />}
-        />
-      )}
     </View>
   );
 };
@@ -114,6 +129,7 @@ export default ExploreScreen;
 const styles = StyleSheet.create({
   view: {
     paddingHorizontal: 16,
-    paddingTop: 20
+    paddingTop: 20,
+    flex:1
   },
 });
