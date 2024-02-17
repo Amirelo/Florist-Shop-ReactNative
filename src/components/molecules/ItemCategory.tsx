513af5d0402cf9b893b9f3255c237bfa8fdcca8f
Amirelo <80700398+Amirelo@users.noreply.github.com>
2024-02-17 19:35:41 +0700

    Fix TextButton not working properly

--- src/components/molecules/ItemCategory.tsx
+++ src/components/molecules/ItemCategory.tsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import {StyleSheet, View, ViewStyle} from 'react-native';
 import themes from '../../themes/themes';
-import {CustomText, CustomImage} from '../atoms';
+import {CustomText, CustomImage, CustomButton} from '../atoms';
 import {CategoryModel} from '../../models';
 
 interface Props {
   //marginTop?: number;
   //marginBottom?: number;
   category: CategoryModel;
+  onPressed?(name:string): void;
 }
 
 const ItemCategory = (props: Props) => {
   return (
-    <View
-      style={
-        [
-          styles.view,
-          //{marginTop: props.marginTop, marginBottom: props.marginBottom},
-        ] as ViewStyle
-      }>
-      <CustomImage type="category" source={props.category.image} />
-      <CustomText color={themes['defaultTheme'].textSecondaryColor}>
-        {props.category.name}
-      </CustomText>
-    </View>
+    <CustomButton>
+      <View
+        style={
+          [
+            styles.view,
+            //{marginTop: props.marginTop, marginBottom: props.marginBottom},
+          ] as ViewStyle
+        }>
+        <CustomImage type="category" source={props.category.image} />
+        <CustomText color={themes['defaultTheme'].textSecondaryColor}>
+          {props.category.name}
+        </CustomText>
+      </View>
+    </CustomButton>
   );
 };
 
--- src/screens/main/screens/HomeScreen.tsx
+++ src/screens/main/screens/HomeScreen.tsx
@@ -9,7 +9,11 @@ import ProductModel from '../../../models/ProductModel';
 import {NavigationProp, useNavigation} from '@react-navigation/native';
 import {getCategories, getProducts} from '../MainService';
 import {faCalendarDay} from '@fortawesome/free-solid-svg-icons';
-import {IMAGE_HOME_HEADER, NAVIGATION_MAIN_ABOUTUS, NAVIGATION_MAIN_PRODUCT_DETAIL} from '../../../constants/AppConstants';
+import {
+  IMAGE_HOME_HEADER,
+  NAVIGATION_MAIN_ABOUTUS,
+  NAVIGATION_MAIN_PRODUCT_DETAIL,
+} from '../../../constants/AppConstants';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import lang from '../../../language/lang';
 import {useSelector} from 'react-redux';
@@ -28,6 +32,10 @@ const HomeScreen = () => {
     (store: any) => store.preference.language,
   );
 
+    const onCategoryPressed = (name:string) => {
+      navigation.navigate(NAVIGATION_MAIN_PRODUCT_DETAIL, {data: {listCategories: [name]}});
+    }
+
   // Navigate to Product Detail Screen with item
   const onProductPressed = (item: ProductModel) => {
     navigation.navigate(NAVIGATION_MAIN_PRODUCT_DETAIL, {item: item});
@@ -35,9 +43,11 @@ const HomeScreen = () => {
 
   // Load data from server
   const waitForData = async () => {
-    const categories: Array<CategoryModel> = await getCategories();
+    const categories: Array<CategoryModel> = (await getCategories()).slice(0,3);
     console.log('category list: ', categories);
-    setListCategories(categories);
+    setListCategories(categories.slice(0,3));
+    const more = new CategoryModel('1','See More','https://images.pexels.com/photos/15387083/pexels-photo-15387083/free-photo-of-letters-on-dice.jpeg')
+    setListCategories([...categories, more])
 
     const products: Array<ProductModel> = await getProducts();
     console.log('product list: ', products);
@@ -46,8 +56,8 @@ const HomeScreen = () => {
 
   // On Need Help Card Pressed
   const onHelpCardPressed = () => {
-    navigation.navigate(NAVIGATION_MAIN_ABOUTUS)
-  }
+    navigation.navigate(NAVIGATION_MAIN_ABOUTUS);
+  };
 
   // Load data first time screen is load
   React.useEffect(() => {
@@ -74,6 +84,9 @@ const HomeScreen = () => {
             marginBottom={24}
             onPressed={onHelpCardPressed}
           />
+
+          <CustomText type='title' marginBottom={20}>Search by occasions</CustomText>
+
           <FlatList
             style={{marginBottom: 32}}
             contentContainerStyle={{
@@ -81,9 +94,9 @@ const HomeScreen = () => {
               width: '100%',
             }}
             horizontal={true}
-            data={listCategories?.slice(0, 4)}
+            data={listCategories}
             keyExtractor={item => item.name}
-            renderItem={({item}) => <ItemCategory category={item} />}
+            renderItem={({item}) => <ItemCategory category={item} onPressed={(name) => onCategoryPressed(name)} />}
           />
 
           <CustomText type="title" marginBottom={24}>
@@ -97,7 +110,7 @@ const HomeScreen = () => {
             showsHorizontalScrollIndicator={false}
             horizontal={true}
             data={listProducts}
-            keyExtractor={item => item.id.toString()}
+            keyExtractor={item => item.id}
             renderItem={({item}) => (
               <ItemProductBig
                 onPressed={() => onProductPressed(item)}
