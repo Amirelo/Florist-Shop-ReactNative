abc758b0f037c59f28e1b5ffc666f312862d7933
Amirelo <80700398+Amirelo@users.noreply.github.com>
2024-02-18 16:21:01 +0700

    theme: missing color

--- src/themes/defaultTheme.tsx
+++ src/themes/defaultTheme.tsx
@@ -2,7 +2,7 @@ const defaultTheme = {
   primaryColor: '#40916c',
   bgColor: '#F4F4F4',
   tertiaryColor: '#ffffff',
-  secondaryColor: '',
+  secondaryColor: '#9B9B9B',
   textColor: '#000000',
   textSecondaryColor: '#9B9B9B',
   errorcolor: '#FF3D3D',
