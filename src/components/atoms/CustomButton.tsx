5352b34eb4a4aa488e995030f8375f4b941962d9
Amirelo <80700398+Amirelo@users.noreply.github.com>
2024-03-25 14:47:48 +0700

    Add animation for Pressable

--- src/components/atoms/CustomButton.tsx
+++ src/components/atoms/CustomButton.tsx
@@ -1,6 +1,6 @@
 // React and libs
 import React from 'react';
-import {Pressable, StyleProp, ViewStyle} from 'react-native';
+import {Animated, Pressable, StyleProp, ViewStyle} from 'react-native';
 
 interface Props {
   onPressed?(): void;
@@ -10,12 +10,34 @@ interface Props {
 }
 
 const CustomButton = (props: Props) => {
+  const animated = new Animated.Value(1);
+
+  const onPressIn = () => {
+    Animated.timing(animated, {
+      toValue: 0.6,
+      duration: 200,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const onPressOut = () => {
+    Animated.timing(animated, {
+      toValue: 1,
+      duration: 200,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
+
   return (
-    <Pressable
-      style={[props.style, {flex: props.flex}]}
-      onPress={props.onPressed}>
+    <AnimatedPressable
+      style={[props.style, {flex: props.flex, opacity: animated}]}
+      onPress={props.onPressed}
+      onPressIn={onPressIn}
+      onPressOut={onPressOut}>
       {props.children}
-    </Pressable>
+    </AnimatedPressable>
   );
 };
 
